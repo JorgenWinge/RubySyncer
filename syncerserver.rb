6a607001e145427fa22d9b89e9f6af8fd290cc86
JorgenWinge <hearttayswift@gmail.com>
2017-05-21 13:13:13 +0200

    Update syncerserver.rb
    Updated the server too with redundancy for commands

--- syncerserver.rb
+++ syncerserver.rb
@@ -4,11 +4,11 @@ require 'win32ole'
 wsh = WIN32OLE.new('Wscript.Shell')
 server = TCPServer.open('192.168.xx.xxx', 3000)  # IPv4 address goes here
 loop {                         # Server stays up in case you need to pause
-   client = server.accept       # Waits until client is launched
-   client.puts("ping")     # Initiate the client code
-   sleep(0.020)            # Add ping delay
-  if wsh.AppActivate('VLC media player') # Add the Windows task bar name of the video player
-    wsh.SendKeys(' ')  # Send the play key
-  end
+  client = server.accept       # Waits until client is launched
+  client.puts("ping")     # Initiate the client code
+  sleep(0.020)            # Add ping delay
+  wsh.SendKeys('+{TAB}')          # Shift + TAB to clear focus
+  wsh.AppActivate('VLC media player')  # Add the taskbar name of the video player you want to activate
+  wsh.SendKeys(' ')                  # Send the play key
   client.close                 # Disconnect the client
 }
