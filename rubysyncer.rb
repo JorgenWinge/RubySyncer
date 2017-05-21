97b947c2224502c3e1d77628df0d966ccc89aa41
JorgenWinge <hearttayswift@gmail.com>
2017-05-21 10:07:37 +0200

    Update rubysyncer.rb
    Workaround for the shortcomings of AppActivate

--- rubysyncer.rb
+++ rubysyncer.rb
@@ -9,9 +9,9 @@ s = TCPSocket.open(hostname, port)
 
 line = s.gets                   # Waits for a line from the server
 if line != 1
-    if wsh.AppActivate('VLC media player') # Add the Windows task bar name of the video player
-    wsh.SendKeys(' ')               # Send the play key
-  end
+  wsh.SendKeys('^{ESC}')             # Unfocus textboxes
+  wsh.AppActivate('VLC media player') # Add the Windows task bar name of the video player
+  wsh.SendKeys(' ')               # Send the play key
 end
 
 s.close                               # Close the socket when done
